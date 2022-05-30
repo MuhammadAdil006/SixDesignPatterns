2333f400727cbebe4f81fee679d8a70b964d6ff1
MuhammadAdil006 <83723564+MuhammadAdil006@users.noreply.github.com>
2022-05-30 21:00:44 +0500

    change of design

---
+++ Design Patterns by AbuBakar Dar/ChainOfReesponsibility/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp22
+{
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+
+        object Handle(object request);
+    }
+
+    // The default chaining behavior can be implemented inside a base handler
+    // class.
+    abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public IHandler SetNext(IHandler handler)
+        {
+            this._nextHandler = handler;
+
+            
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            if (this._nextHandler != null)
+            {
+                return this._nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    class MonkeyHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if ((request as string) == "Banana")
+            {
+                return $"Monkey: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class SquirrelHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "Nut")
+            {
+                return $"Squirrel: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class DogHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "MeatBall")
+            {
+                return $"Dog: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class Client
+    {
+        
+        public static void ClientCode(AbstractHandler handler)
+        {
+            foreach (var food in new List<string> { "Nut", "Banana", "Cup of coffee" })
+            {
+                Console.WriteLine($"Client: Who wants a {food}?");
+
+                var result = handler.Handle(food);
+
+                if (result != null)
+                {
+                    Console.Write($"   {result}");
+                }
+                else
+                {
+                    Console.WriteLine($"   {food} was left untouched.");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The other part of the client code constructs the actual chain.
+            var monkey = new MonkeyHandler();
+            var squirrel = new SquirrelHandler();
+            var dog = new DogHandler();
+
+            monkey.SetNext(squirrel).SetNext(dog);
+
+            
+            Client.ClientCode(monkey);
+            Console.WriteLine();
+
+           
+        }
+    }
+}
